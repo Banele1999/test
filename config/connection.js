50ed0f6d000b5b41e67d9fb77831acdb22971d83
unknown <joel.buti97@gmail.com>
2023-11-24 08:47:22 +0200

    update

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,31 @@
-var mysql =  require("mysql");
+var mysql = require("mysql");
 
 //Create Connections
+/*
 var mysqlConnection = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: '',
     database: "hackacity"
 })
+*/
+var mysqlConnection = mysql.createConnection({
+    host: "sql8.freemysqlhosting.net",
+    user: "sql8664506",
+    password: 'wMsRHw59gF',
+    database: "sql8664506",
+    port: '3306'
+})
 
 //connect to database
 mysqlConnection.connect((err) => {
-    if (err){
+    if (err) {
         console.log(err);
-    }else{
+    } else {
         console.log('connection succeded');
     }
+    //mysqlConnection.end();
 })
 
 
-module.exports =mysqlConnection ;
+module.exports = mysqlConnection;
